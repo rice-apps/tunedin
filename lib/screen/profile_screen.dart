97db63120091e4e677dda4138b23c4bbc0cf1b5c
AhmedA1559 <95593064+AhmedA1559@users.noreply.github.com>
2022-10-16 18:25:33 -0500

    Add basic UI for profile screen
    Co-authored-by: Vivian-Xiao1025 <Vivian-Xiao1025@users.noreply.github.com>

--- lib/screen/profile_screen.dart
+++ lib/screen/profile_screen.dart
@@ -1,16 +1,96 @@
 import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatelessWidget {
-  const ProfileScreen({Key? key}) : super(key: key);
+  ProfileScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Profile'),
-      ),
-      body: const Center(
-        child: Text('Profile'),
+        body: Column(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        const CircleAvatar(
+          backgroundImage: NetworkImage(
+            "https://picsum.photos/250?image=9",
+          ),
+          radius: 75,
+        ),
+        const Text("Will Rice College"),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Column(
+              children: [
+                const Text("208"),
+                const Text("Followers"),
+              ],
+            ),
+            Column(
+              children: [
+                const Text("80"),
+                const Text("Following"),
+              ],
+            ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            OutlinedButton(
+              onPressed: () => {},
+              child: const Text("Follow"),
+            ),
+            OutlinedButton(
+              onPressed: () => {},
+              child: const Text("Join"),
+            ),
+          ],
+        ),
+        Column(
+          children: [
+            const Text("Playlists: 4"),
+            PlaylistCard(),
+          ],
+        )
+      ],
+    ));
+  }
+}
+
+class PlaylistCard extends StatelessWidget {
+  const PlaylistCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Row(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(16),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(8.0),
+            child: Image.network(
+              "https://picsum.photos/250?image=9",
+              width: 48,
+              height: 48,
+              fit: BoxFit.cover,
+            ),
+          ),
+          ),
+          Column(
+            children: [
+              const Text("Playlist Name"),
+              const Text("Playlist Description"),
+            ],
+          ),
+          const Padding(
+            padding: EdgeInsets.fromLTRB(50, 0, 0, 0),
+            child: Icon(Icons.more_vert),
+          ),
+        ],
       ),
     );
   }
