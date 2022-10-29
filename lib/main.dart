bd20cad83b41af7b4135cf67509d6441e6ec0e92
Anya Gu <anyagu@Anyas-MacBook-Air.local>
2022-10-29 14:52:56 -0500

    install dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,22 @@
 import 'package:flutter/material.dart';
 import 'widgets/my_navigation_bar.dart';
 
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flutter_appauth/flutter_appauth.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+final FlutterAppAuth appAuth = FlutterAppAuth();
+final FlutterSecureStorage secureStorage = const FlutterSecureStorage();
+
+// Auth0 Variables
+
+const AUTH0_DOMAIN = 'dev-kt5qj3uqxps4pfhg.us.auth0.com';
+const AUTH0_CLIENT_ID = 'BHLNHUjsA4IQbAWL7WlX6ccJtVVJCojR';
+
+const AUTH0_REDIRECT_URI = 'com.example.tunedin://login-callback';
+const AUTH0_ISSUER = 'https://$AUTH0_DOMAIN';
+
 void main() {
   runApp(const MyApp());
 }
