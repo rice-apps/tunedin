9ca983990807936b16d94b8cf49234c26759a84c
shannonshao <90943847+shannonshao@users.noreply.github.com>
2022-10-29 14:51:25 -0500

    Remove comments

--- lib/models/post_model.dart
+++ lib/models/post_model.dart
@@ -17,31 +17,3 @@ class PostModel with _$PostModel {
   factory PostModel.fromJson(Map<String, dynamic> json) =>
       _$PostModelFromJson(json);
 }
-
-// class PostModel {
-//   String netID = "";
-//   int numLikes = 0;
-//   String bodyText = "";
-//   var comments = [];
-//   String musicURL = "";
-//   // ...
-
-//   PostModel(
-//       {required this.netID, required this.bodyText, required this.musicURL});
-
-//   PostModel.fromJson(Map<String, dynamic> json) {
-//     netID = json['netID'];
-//     numLikes = json['numLikes'];
-//     comments = json['comments'];
-//     musicURL = json['musicURL'];
-//   }
-
-//   Map<String, dynamic> toJson() {
-//     final data = <String, dynamic>{};
-//     data['netID'] = netID;
-//     data['numLikes'] = numLikes;
-//     data['comments'] = comments;
-//     data['musicURL'] = musicURL;
-//     return data;
-//   }
-// }
