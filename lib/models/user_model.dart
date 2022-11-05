6e1869b6b286b3c99fa184bf66b5e7d567155afa
Daniel Cufino <dc58@rice.edu>
2022-11-05 13:26:15 -0500

    Making requested changes

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,31 +1,20 @@
 import 'package:json_annotation/json_annotation.dart';
 
-/// Class with represents the user model.
-///
-/// Holds all data pertaining to a single user.
+
 @JsonSerializable(explicitToJson: true)
 class UserModel {
   String? netID;
   String? displayName;
   String? userName;
-  // String? email;
-  // String? college;
+
   List<UserModel>? following;
 
   UserModel({
     required this.netID,
     required this.displayName,
     required this.userName,
-    // required this.email,
-    // required this.college,
     required this.following,
   });
-
-  //User(this.name, this.email, this.userName, this.college)
-
-  // UserModel(this.email, this.userName, this.name, this.College);
-
-  //User.fromJson(Map<String, dynamic> json)
 
   factory UserModel.fromJson(Map<String, dynamic> json) {
     return UserModel(
@@ -37,9 +26,7 @@ class UserModel {
 
   Map<String, dynamic> toJson() => {
         'name': displayName,
-        // 'email': email,
         'userName': userName,
-        // 'college': college,
         'following': following
       };
   void followUser(UserModel targetUser) {
