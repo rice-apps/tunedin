013d6b4efb64495db0a57cb3f9f5f3ed75ba99da
Shreyas Minocha <sm-git@riseup.net>
2022-11-05 02:01:39 -0500

    Add minimal koa server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,12 @@
-console.log('hello world');
+import Koa from 'koa';
+import Router from '@koa/router';
+
+const app = new Koa();
+const router = new Router();
+
+router.get('/', (ctx, next) => {
+	ctx.body = {};
+});
+
+app.use(router.routes());
+app.listen(3000);
