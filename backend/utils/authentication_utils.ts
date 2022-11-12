eabc4c6f56cef8dd0dc540d39a9b7d1689beaa1d
Anya Gu <anyagu@Anyas-MacBook-Air.local>
2022-11-12 14:43:37 -0600

    copy pasted auth from carpool

---
+++ backend/utils/authentication_utils.ts
@@ -0,0 +1,95 @@
+// This will be created when the user logs in
+const jwt = require('jsonwebtoken');
+
+import axios from 'axios';
+
+var xml2js = require('xml2js');
+var stripPrefix = require('xml2js').processors.stripPrefix;
+
+import { SECRET, SERVICE_URL } from '../config';
+import user from '../src/models/user';
+
+/**
+ * Parser used for XML response by CAS
+ */
+const parser = new xml2js.Parser({
+    tagNameProcessors: [stripPrefix],
+    explicitArray: false
+});
+
+/**
+ * Default failure response when authentication / verification doesn't work.
+ */
+const failureResponse = { success: false };
+
+let config = {
+    CASValidateURL: 'https://idp.rice.edu/idp/profile/cas/serviceValidate',
+}
+
+/**
+ * Given a user, creates a new token for them.
+ */
+export const createToken = (user) => {
+    let token = jwt.sign({
+        id: user._id,
+        netid: user.netid
+    }, SECRET, { expiresIn: "12h" });
+    return token;
+}
+
+/**
+ * Given a token, finds the associated user.
+ */
+export const getUserFromToken = async (token) => {
+    let user = await User.find({ token: token });
+    return user;
+}
+
+/**
+ * Given a token, verifies that it is still valid.
+ */
+export const verifyToken = async (token) => {
+    try {
+        // In the future, we may need the other properties...
+        let { id, netid, iat, exp } = await jwt.verify(token, SECRET);
+        return { success: true, id };
+    } catch (e) {
+        return failureResponse;
+    }
+}
+
+/**
+ * Given a ticket, authenticates it and returns the corresponding netid of the now-authenticated user.
+ */
+export const authenticateTicket = async (ticket) => {
+    try {
+        // validate our ticket against the CAS server
+        var url = `${config.CASValidateURL}?ticket=${ticket}&service=${SERVICE_URL}`;
+
+        // First validate ticket against CAS, get a data object back
+        let { data } = await axios.get(url);
+
+        // Parse returned XML data with xml2js parser
+        return parser.parseStringPromise(data).then(
+            (parsedResponse) => {
+                let serviceResponse = parsedResponse.serviceResponse;
+                // This object contains the information as to whether this login was successful
+                var authSucceded = serviceResponse.authenticationSuccess;
+                if (authSucceded) {
+                    // authSucceded.user is the netid
+                    let netid = authSucceded.user;
+                    return { success: true, netid };
+                } else {
+                    return failureResponse;
+                }
+            },
+            (err) => {
+                console.log("Error!");
+                return failureResponse;
+            }
+        );
+    } catch (e) {
+        console.log("Something went wrong.");
+        return failureResponse;
+    }
+}
