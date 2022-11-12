e0d7c1e8d40e9883be25d1f05c05abc666a8cb3f
shannonshao <90943847+shannonshao@users.noreply.github.com>
2022-11-12 13:20:27 -0600

    unique usernames

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,22 @@
-console.log('hello world');
+import Koa from 'koa';
+import Router from '@koa/router';
+import 'reflect-metadata';
+import db from './db';
+import userRouter from './routes/user';
+
+await db.initialize();
+
+const app = new Koa();
+const indexRouter = new Router();
+
+indexRouter.get('/', (ctx, next) => {
+	ctx.body = {};
+});
+
+app.use(indexRouter.routes());
+app.use(indexRouter.allowedMethods());
+
+app.use(userRouter.routes());
+app.use(userRouter.allowedMethods());
+
+app.listen(3000);
