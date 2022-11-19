ae276642738fb39fe060a89f524ba6190f51c43f
AhmedA1559 <95593064+AhmedA1559@users.noreply.github.com>
2022-11-19 13:40:39 -0600

    Fix Profile Screen to include analyze fixes

--- lib/screen/profile_screen.dart
+++ lib/screen/profile_screen.dart
@@ -4,10 +4,10 @@ class ProfileScreen extends StatefulWidget {
   const ProfileScreen({Key? key}) : super(key: key);
 
   @override
-  _ProfileScreenState createState() => _ProfileScreenState();
+  ProfileScreenState createState() => ProfileScreenState();
 }
 
-class _ProfileScreenState extends State<ProfileScreen>
+class ProfileScreenState extends State<ProfileScreen>
     with TickerProviderStateMixin {
   late TabController _tabController;
 
@@ -136,8 +136,8 @@ class _ProfileScreenState extends State<ProfileScreen>
             ),
             Column(
               crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
+              children: const [
+                Text(
                   "1 day ago",
                   textAlign: TextAlign.left,
                 ),
@@ -145,7 +145,7 @@ class _ProfileScreenState extends State<ProfileScreen>
                 InfoCard(),
                 InfoCard(),
                 InfoCard(),
-                const SizedBox(height: 25),
+                SizedBox(height: 25),
               ],
             ),
           ][_tabController.index],
@@ -155,7 +155,6 @@ class _ProfileScreenState extends State<ProfileScreen>
   }
 }
 
-
 class PlaylistCard extends StatelessWidget {
   const PlaylistCard({Key? key}) : super(key: key);
 
@@ -164,52 +163,53 @@ class PlaylistCard extends StatelessWidget {
     return Card(
       child: InkWell(
           onTap: () {},
-          child: InfoCard(hasChevron: true,)),
+          child: const InfoCard(
+            hasChevron: true,
+          )),
     );
   }
 }
+
 class InfoCard extends StatelessWidget {
-  bool hasChevron;
-  InfoCard({this.hasChevron=false, Key? key}) : super(key: key);
+  final bool hasChevron;
+  const InfoCard({this.hasChevron = false, Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Row(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(16),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(8.0),
-                child: Image.network(
-                  "https://picsum.photos/250?image=9",
-                  width: 48,
-                  height: 48,
-                  fit: BoxFit.cover,
-                ),
-              ),
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(16),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(8.0),
+            child: Image.network(
+              "https://picsum.photos/250?image=9",
+              width: 48,
+              height: 48,
+              fit: BoxFit.cover,
             ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: const [
-                Text(
-                  "Playlist Name",
-                  textAlign: TextAlign.left,
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                Text("20 Songs", textAlign: TextAlign.left),
-              ],
+          ),
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            Text(
+              "Playlist Name",
+              textAlign: TextAlign.left,
             ),
-            if (hasChevron) 
-              const Spacer(),
-            if (hasChevron) 
-              const Padding(
-                padding: EdgeInsets.fromLTRB(0, 0, 10, 0),
-                child: Icon(Icons.chevron_right),
-              ),
-            
+            SizedBox(
+              height: 5,
+            ),
+            Text("20 Songs", textAlign: TextAlign.left),
           ],
-        );
+        ),
+        if (hasChevron) const Spacer(),
+        if (hasChevron)
+          const Padding(
+            padding: EdgeInsets.fromLTRB(0, 0, 10, 0),
+            child: Icon(Icons.chevron_right),
+          ),
+      ],
+    );
   }
 }
