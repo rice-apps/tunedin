c172f9eff6a6c3c07182a38769395a709987aacf
AhmedA1559 <95593064+AhmedA1559@users.noreply.github.com>
2022-12-25 22:17:24 -0600

    Add home screen app bar

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -8,6 +8,29 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Theme.of(context).colorScheme.background,
+        title: Text(
+          "TunedIn",
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(
+              Icons.add,
+            ),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: const Icon(
+              Icons.notifications,
+            ),
+            onPressed: () {},
+          ),
+        ],
+      ),
       body:
           ListView.builder(itemBuilder: (context, index) => const PostWidget()),
     );
