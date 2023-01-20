cbe459af62fce757c198a465a758e2dd620c4db0
Daniel Cufino <71530918+DanielCufino@users.noreply.github.com>
2023-01-20 16:06:51 -0600

    Spotify API integration and dotenv (#19)
    * work in progress
    
    * Add Spotify API Routes
    
    * Auth Flow Complete
    
    Front-end should use URL within JSON to give app permissions.
    
    Co-Authored-By: Nolan Tuite <48373732+Nolan2IT@users.noreply.github.com>
    
    * Create example .env
    
    Co-Authored-By: Nolan Tuite <48373732+Nolan2IT@users.noreply.github.com>
    
    * Update .env example
    
    Co-Authored-By: Nolan Tuite <48373732+Nolan2IT@users.noreply.github.com>
    
    * Setup .env
    
    This task #20
    
    Co-Authored-By: Nolan Tuite <48373732+Nolan2IT@users.noreply.github.com>
    
    * fix mongodb connection + minor edits
    
    * lint
    
    * fix missing npm package
    
    Co-authored-by: Nolan Tuite <48373732+Nolan2IT@users.noreply.github.com>
    Co-authored-by: Baylee Keevan <bjk9@rice.edu>

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import Router from '@koa/router';
 import 'reflect-metadata';
 import db from './db';
 import userRouter from './routes/user';
+import spotifyRouter from './routes/spotify';
 import postRouter from './routes/post';
 
 await db.initialize();
@@ -11,12 +12,18 @@ const PORT = 3000;
 
 const app = new Koa();
 const indexRouter = new Router();
+indexRouter.get('/', (ctx, next) => {
+	ctx.body = {};
+});
 
 app.use(indexRouter.routes());
 app.use(indexRouter.allowedMethods());
 
 app.use(userRouter.routes());
 app.use(userRouter.allowedMethods());
+
+app.use(spotifyRouter.routes());
+app.use(spotifyRouter.allowedMethods());
 
 app.use(postRouter.routes());
 app.use(postRouter.allowedMethods());
---
+++ backend/src/routes/spotify.ts
@@ -0,0 +1,84 @@
+import SpotifyWebApi from 'spotify-web-api-node';
+import Router from '@koa/router';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+var spotifyApi = new SpotifyWebApi({
+	clientId: process.env.CLIENT_ID,
+	clientSecret: process.env.CLIENT_SECRET,
+	redirectUri: process.env.REDIRECT_URI,
+});
+
+const scopes = [
+	'ugc-image-upload',
+	'user-read-playback-state',
+	'user-modify-playback-state',
+	'user-read-currently-playing',
+	'streaming',
+	'app-remote-control',
+	'user-read-email',
+	'user-read-private',
+	'playlist-read-collaborative',
+	'playlist-modify-public',
+	'playlist-read-private',
+	'playlist-modify-private',
+	'user-library-modify',
+	'user-library-read',
+	'user-top-read',
+	'user-read-playback-position',
+	'user-read-recently-played',
+	'user-follow-read',
+	'user-follow-modify',
+];
+
+const router = new Router({
+	prefix: '/spotify',
+});
+
+router.get('/login', async (ctx, next) => {
+	ctx.body = {
+		//Retrieves the login url and send to front-end.
+		url: spotifyApi.createAuthorizeURL(scopes),
+	};
+});
+
+router.get('/callback', async (ctx, next) => {
+	const error = ctx.query.error;
+	const code = ctx.query.code;
+	const state = ctx.query.state;
+
+	if (error) {
+		console.error('Callback Error:', error);
+		ctx.body = `Callback Error: ${error}`;
+		return;
+	}
+	const authData = await spotifyApi
+		.authorizationCodeGrant(code)
+		.catch((error) => {
+			console.error('Error getting Auth Tokens:', error);
+			ctx.body = `Error getting Auth Tokens: ${error}`;
+			return;
+		});
+
+	const access_token = authData.body['access_token'];
+	const refresh_token = authData.body['refresh_token'];
+	const expires_in = authData.body['expires_in'];
+
+	console.log('Access Token:', access_token);
+	console.log('Refresh Token:', refresh_token);
+	console.log(
+		`Sucessfully retreived access token. Expires in ${expires_in} s.`
+	);
+
+	ctx.body = 'Success! You can now close the window.';
+
+	//Refresh the access token before it expires
+	setInterval(async () => {
+		const data = await spotifyApi.refreshAccessToken();
+		const access_token = data.body['access_token'];
+
+		spotifyApi.setAccessToken(access_token);
+	}, (expires_in / 2) * 1000);
+});
+
+export default router;
