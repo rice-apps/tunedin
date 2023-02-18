c26bcc8d6bd33d20d2d4a5835cca94866726a31c
Vivian-Xiao1025 <67659378+Vivian-Xiao1025@users.noreply.github.com>
2023-02-18 13:27:28 -0600

    "combined feature"

--- lib/screen/profile_screen.dart
+++ lib/screen/profile_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 
 import '../widgets/my_navigation_bar.dart';
 import '../widgets/outlined_button.dart';
+import '../widgets/playlist_card.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({Key? key}) : super(key: key);
@@ -14,6 +15,8 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen>
     with TickerProviderStateMixin {
   late TabController _tabController;
+  
+  int numSongs = 15;
 
   @override
   void initState() {
@@ -162,28 +165,8 @@ class _ProfileScreenState extends State<ProfileScreen>
               <Widget>[
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
-                  children: const [
-                    Label(name: "Songs", count: "120"),
-                    PlaylistCard(),
-                    PlaylistCard(),
-                    PlaylistCard(),
-                    PlaylistCard(),
-                    PlaylistCard(),
-                    PlaylistCard(),
-                    PlaylistCard(),
-                    PlaylistCard(),
-                    PlaylistCard(),
-                    PlaylistCard(),
-                    PlaylistCard(),
-                    PlaylistCard(),
-                    PlaylistCard(),
-                    PlaylistCard(),
-                    PlaylistCard(),
-                    PlaylistCard(),
-                    PlaylistCard(),
-                    PlaylistCard(),
-                    PlaylistCard(),
-                    PlaylistCard(),
+                  children: [
+                    Label(name: "Songs", count: numSongs.toString()),
                     PlaylistCard(),
                     PlaylistCard(),
                     PlaylistCard(),
@@ -210,110 +193,6 @@ class _ProfileScreenState extends State<ProfileScreen>
           ),
         ),
       ]),
-    );
-  }
-}
-
-class Label extends StatelessWidget {
-  final String name;
-  final String count;
-
-  const Label({Key? key, required this.name, required this.count})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(5, 0, 0, 10),
-      child: RichText(
-          text: TextSpan(
-        text: '$name ',
-        style: Theme.of(context).textTheme.headlineMedium,
-        children: [
-          const WidgetSpan(
-              alignment: PlaceholderAlignment.baseline,
-              baseline: TextBaseline.alphabetic,
-              child: SizedBox(width: 5)),
-          TextSpan(
-            text: count,
-            style: Theme.of(context).textTheme.headlineMedium?.copyWith(
-                  color: const Color(0xFFA1A9BC),
-                  fontWeight: FontWeight.w400,
-                ),
-          ),
-        ],
-      )),
-    );
-  }
-}
-
-class PlaylistCard extends StatelessWidget {
-  const PlaylistCard({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      child: InkWell(
-          onTap: () {},
-          child: const InfoCard(
-            hasChevron: true,
-          )),
-    );
-  }
-}
-
-class InfoCard extends StatelessWidget {
-  final bool hasChevron;
-
-  const InfoCard({
-    Key? key,
-    this.hasChevron = false,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(12),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(8.0),
-            child: Image.network(
-              "https://picsum.photos/250?image=9",
-              width: 56,
-              height: 56,
-              fit: BoxFit.cover,
-            ),
-          ),
-        ),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              "Playlist Name",
-              textAlign: TextAlign.left,
-              style: Theme.of(context)
-                  .textTheme
-                  .displayMedium
-                  ?.copyWith(color: Colors.white),
-            ),
-            const SizedBox(
-              height: 5,
-            ),
-            Text("20 Songs",
-                textAlign: TextAlign.left,
-                style: Theme.of(context).textTheme.displayMedium?.copyWith(
-                      color: const Color(0xFFA1A9BC),
-                    )),
-          ],
-        ),
-        if (hasChevron) const Spacer(),
-        if (hasChevron)
-          const Padding(
-            padding: EdgeInsets.fromLTRB(0, 0, 10, 0),
-            child: Icon(Icons.chevron_right),
-          ),
-      ],
     );
   }
 }
