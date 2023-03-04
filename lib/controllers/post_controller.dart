f00a6cdb40329ae1a63d3ae9fca6b92a8d44a060
Ahmed A <95593064+AhmedA1559@users.noreply.github.com>
2023-03-04 14:53:27 -0600

    Add ability to send post-related requests (#31)

---
+++ lib/controllers/post_controller.dart
@@ -0,0 +1,30 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../providers/post_provider.dart';
+
+class PostController {
+  final WidgetRef _ref;
+
+  PostController(this._ref);
+
+  Future<void> likePost() async {
+    final postId = _ref
+        .read(postStateProvider.notifier)
+        .state
+        .netID; // replace this with postID
+    _ref.read(postRepositoryProvider).likePost(postId).then((value) =>
+        _ref.read(postStateProvider.notifier).state = _ref
+            .read(postStateProvider.notifier)
+            .state
+            .copyWith(numLikes: value.numLikes + 1));
+  }
+
+  Future<void> deletePost() async {
+    final postId = _ref
+        .read(postStateProvider.notifier)
+        .state
+        .netID; // replace this with postID
+    _ref.read(postRepositoryProvider).deletePost(postId).then((value) =>
+        _ref.read(postStateProvider.notifier).status = PostStatus.deleted);
+  }
+}
---
+++ lib/providers/post_provider.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:rice_music_sharing/providers/http_provider.dart';
+
+import '../models/post_model.dart';
+import '../repository/post_repository.dart';
+
+final postRepositoryProvider = Provider<PostRepository>(
+    (ref) => PostRepository(postApiClient: ref.read(httpProvider)));
+final postListProvider = FutureProvider.autoDispose<List<PostModel>>(
+    (ref) => ref.read(postRepositoryProvider).fetchPosts());
+
+enum PostStatus { loading, loaded, error, deleted }
+
+class PostState extends StateNotifier<PostModel> {
+  PostStatus _status = PostStatus.loading;
+  PostState(PostModel model) : super(model);
+
+  PostStatus get status => _status;
+  set status(PostStatus status) => _status = status;
+}
+
+final postStateProvider =
+    StateNotifierProvider.autoDispose<PostState, PostModel>(
+        throw UnimplementedError());
