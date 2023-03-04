3434caa471d27eb60e74f0efbc46d38dd188f0a7
Ahmed A <95593064+AhmedA1559@users.noreply.github.com>
2023-03-04 14:39:16 -0600

    Change the base url

--- lib/http/api_client.dart
+++ lib/http/api_client.dart
@@ -4,7 +4,7 @@ class ApiClient {
   final Dio _dio = Dio(
     BaseOptions(
       baseUrl:
-          'https://tunedin-api.herokuapp.com', // TO:DO change this to an actual base url
+          '', // TO:DO change this to an actual base url
     ),
   );
 
