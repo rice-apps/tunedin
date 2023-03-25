4c9dfbb8d810ba381ee336477a7028d7ba90f264
AhmedA1559 <95593064+AhmedA1559@users.noreply.github.com>
2023-03-25 13:34:49 -0500

    Add formating as well

--- lib/controllers/post_controller.dart
+++ lib/controllers/post_controller.dart
@@ -8,17 +8,18 @@ class PostController {
   PostController(this._ref);
 
   Future<void> likePost() async {
-    final postId = _ref
-        .read(postStateProvider).netID; // replace this with postID
-    _ref.read(postRepositoryProvider).likePost(postId).then((value) =>
-        _ref.read(postStateProvider.notifier).setState(_ref
+    final postId =
+        _ref.read(postStateProvider).netID; // replace this with postID
+    _ref.read(postRepositoryProvider).likePost(postId).then((value) => _ref
+        .read(postStateProvider.notifier)
+        .setState(_ref
             .read(postStateProvider)
             .copyWith(numLikes: value.numLikes + 1)));
   }
 
   Future<void> deletePost() async {
-    final postId = _ref
-        .read(postStateProvider).netID; // replace this with postID
+    final postId =
+        _ref.read(postStateProvider).netID; // replace this with postID
     _ref.read(postRepositoryProvider).deletePost(postId).then((value) =>
         _ref.read(postStateProvider.notifier).status = PostStatus.deleted);
   }
